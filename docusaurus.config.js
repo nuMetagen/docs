d50dfe9e6d604b9948298fd21a27fb9e534dd52f
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2022-12-30 16:57:41 +0700

    update github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,43 +1,43 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'nuMetagen SDK',
-  tagline: 'Documentation for nuMetagen AR SDK',
+  title: "nuMetagen SDK",
+  tagline: "Documentation for nuMetagen AR SDK",
 
-  url: 'https://docs.numetagen.id',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/logo.png',
+  url: "https://docs.numetagen.id",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/logo.png",
 
-  organizationName: 'nuMetagen',
-  projectName: 'docs',
-  deploymentBranch: 'gh-pages',
+  organizationName: "nuMetagen",
+  projectName: "docs",
+  deploymentBranch: "gh-pages",
   trailingSlash: false,
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
           // editUrl: 'https://github.com/nuMetagen/docs/tree/master/',
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -46,28 +46,28 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'nuMetagen',
+        title: "nuMetagen",
         logo: {
-          alt: 'nuMetagen Logo',
-          src: 'img/logo.png',
+          alt: "nuMetagen Logo",
+          src: "img/logo.png",
         },
         hideOnScroll: true,
-        items: [{
-            type: 'doc',
-            docId: 'Introduction/getting-started',
-            position: 'right',
-            label: 'Docs',
+        items: [
+          {
+            type: "doc",
+            docId: "Introduction/getting-started",
+            position: "right",
+            label: "Docs",
           },
           {
-            href: 'https://github.com/nuMetagen/',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/nuMetagen/docs",
+            label: "GitHub",
+            position: "right",
           },
-          
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           // {
           //   title: 'Docs',
